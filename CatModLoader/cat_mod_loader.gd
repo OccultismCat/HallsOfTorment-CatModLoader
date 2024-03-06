d29b84db9b36ced80f593e606b55bf33aa48f095
OccultismCat <96416764+OccultismCat@users.noreply.github.com>
2024-03-06 03:59:48 -0500

    forgot to disable quick play & autoplayer again.

--- CatModLoader/cat_mod_loader.gd
+++ CatModLoader/cat_mod_loader.gd
@@ -258,7 +258,7 @@ func print_mod_controls():
 
 func _ready():
 	if mods_loaded == false:
-		toggle_autoplayer(true)
+		toggle_autoplayer(false)
 		get_all_mods()
 		print_loader_text()
 	
@@ -266,7 +266,7 @@ func _process(delta):
 	if GameState.CurrentState == GameState.States.Overworld and get_current_scene() != null:
 		if mods_loaded == false:
 			print_mod_controls()
-			quick_play()
+			#quick_play()
 			#set_game_state(GameState.States.RegisterOfHalls)
 		mods_loaded = true
 	if mods_loaded == true:
